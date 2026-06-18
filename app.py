4b6e9099e9c1aa04550f569dacac70f43fb58946
Anusha <bellabebo48@gmail.com>
2026-06-18 05:13:18 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/message")
+def message():
+    return render_template("message.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
